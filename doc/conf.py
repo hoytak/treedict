d4b4105e03eb193f6258a924a5d7bfabddeebed0
Hoyt Koepke <hoytak@gmail.com>
2011-09-05 11:48:34 -0700

    Changed documentation html theme.

--- doc/conf.py
+++ doc/conf.py
@@ -90,7 +90,8 @@ pygments_style = 'sphinx'
 # Options for HTML output
 # -----------------------
 
-html_theme = "agogo"
+html_theme = "hoytwww"
+html_theme_path = ["_themes"]
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
